1df9a0f0ce3746f26ee312c303a9963cb0006018
Miguel Schütz <schuetzmigueldev@gmail.com>
2026-02-21 16:33:43 +0100

    :bug Add onDeleteOrUpdate to improve database handling

--- Appointments.Database/Migration/M001InitialMigration.cs
+++ Appointments.Database/Migration/M001InitialMigration.cs
@@ -1,49 +1,50 @@
+using FluentMigrator;
+
 namespace Appointments.Database.Migration;
 
+[Migration(1771178306)]
 public class M001InitialMigration : FluentMigrator.Migration
 {
     public override void Up()
     {
-        Create.Table("persons")
-            .WithColumn("id").AsGuid().NotNullable().PrimaryKey().WithDefaultValue(Guid.NewGuid())
-            .WithColumn("firstname").AsString().NotNullable()
-            .WithColumn("lastname").AsString().NotNullable();
-
+        
+        Execute.Sql("CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";"); 
+        
         Create.Table("organizations")
-            .WithColumn("id").AsGuid().NotNullable().PrimaryKey().WithDefaultValue(Guid.NewGuid())
+            .WithColumn("id").AsGuid().NotNullable().PrimaryKey().WithDefault(SystemMethods.NewGuid)
             .WithColumn("name").AsString().NotNullable();
 
         Create.Table("employees")
-            .WithColumn("id").AsGuid().NotNullable().PrimaryKey().WithDefaultValue(Guid.NewGuid())
+            .WithColumn("id").AsGuid().NotNullable().PrimaryKey().WithDefault(SystemMethods.NewGuid)
             .WithColumn("person_id").AsGuid().NotNullable().ForeignKey("persons", "id");
 
         Create.Table("organization_employees")
-            .WithColumn("id").AsGuid().NotNullable().PrimaryKey().WithDefaultValue(Guid.NewGuid())
+            .WithColumn("id").AsGuid().NotNullable().PrimaryKey().WithDefault(SystemMethods.NewGuid)
             .WithColumn("organization_id").AsGuid().NotNullable().ForeignKey("organizations", "id")
             .WithColumn("employee_id").AsGuid().NotNullable().ForeignKey("employees", "id");
 
         Create.Table("appointments")
-            .WithColumn("id").AsGuid().NotNullable().PrimaryKey().WithDefaultValue(Guid.NewGuid())
+            .WithColumn("id").AsGuid().NotNullable().PrimaryKey().WithDefault(SystemMethods.NewGuid)
             .WithColumn("name").AsGuid().NotNullable()
             .WithColumn("start_time").AsDateTime().NotNullable()
             .WithColumn("end_time").AsDateTime().NotNullable();
 
         Create.Table("customers")
-            .WithColumn("id").AsGuid().NotNullable().PrimaryKey().WithDefaultValue(Guid.NewGuid())
+            .WithColumn("id").AsGuid().NotNullable().PrimaryKey().WithDefault(SystemMethods.NewGuid)
             .WithColumn("person_id").AsGuid().NotNullable().ForeignKey("persons", "id")
             .WithColumn("appointment_id").AsGuid().NotNullable().ForeignKey("appointments", "id");
 
         Create.Table("appointment_employees")
-            .WithColumn("id").AsGuid().NotNullable().PrimaryKey().WithDefaultValue(Guid.NewGuid())
+            .WithColumn("id").AsGuid().NotNullable().PrimaryKey().WithDefault(SystemMethods.NewGuid)
             .WithColumn("appointment_id").AsGuid().NotNullable().ForeignKey("appointments", "id")
             .WithColumn("employee_id").AsGuid().NotNullable().ForeignKey("employees", "id");
 
         Create.Table("kinds")
-            .WithColumn("id").AsGuid().NotNullable().PrimaryKey().WithDefaultValue(Guid.NewGuid())
+            .WithColumn("id").AsGuid().NotNullable().PrimaryKey().WithDefault(SystemMethods.NewGuid)
             .WithColumn("name").AsString().NotNullable();
 
         Create.Table("appointment_kinds")
-            .WithColumn("id").AsGuid().NotNullable().PrimaryKey().WithDefaultValue(Guid.NewGuid())
+            .WithColumn("id").AsGuid().NotNullable().PrimaryKey().WithDefault(SystemMethods.NewGuid)
             .WithColumn("appointment_id").AsGuid().NotNullable().ForeignKey("appointments", "id")
             .WithColumn("kind_id").AsGuid().NotNullable().ForeignKey("kinds", "id");
     }
